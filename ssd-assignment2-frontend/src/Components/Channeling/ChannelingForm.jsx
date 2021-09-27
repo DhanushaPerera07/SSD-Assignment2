19ac030b7a51de18cca810cbae48546ea3cea9c8
Janaka Chinthana <janakachinthana1@gmail.com>
2021-09-28 00:29:42 +0530

    first commit

--- ssd-assignment2-frontend/src/Components/Channeling/ChannelingForm.jsx
+++ ssd-assignment2-frontend/src/Components/Channeling/ChannelingForm.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Card } from 'react-bootstrap';
 
 export default class ChannelingForm extends React.Component {
     render() {
         return (
             <div>
-                <h1 style={{ marginTop: '10%' }}>Enter patient's details</h1>
+                <h1 style={{ marginTop: '5%' }}>Enter patient's details</h1>
                 <hr></hr>
-                <Form >
+                <div className="row"> 
+                    <div className="col-md-4"></div>
+                    <div className="col-md-4">
+                        <Card className="container">
+                            <br/>
+                        <Form >
                     <Form.Group className="mb-3" controlId="formBasicName">
                         <Form.Label>Patient Name</Form.Label>
                         <Form.Control type="name" placeholder="Enter Namel" />
@@ -21,7 +26,7 @@ export default class ChannelingForm extends React.Component {
                     <Form.Group controlId="formBasicDelivery">
                         <Form.Label >Select a Doctor</Form.Label> <br></br>
                         <Form.Control name="doctorName" as="select"
-                            custom
+                            custom  className="form-control"
                             >
                             <option value="Select">Select a Doctor</option>
 
@@ -39,12 +44,19 @@ export default class ChannelingForm extends React.Component {
                         <Form.Label>Insert an image of your payment slip</Form.Label><br></br>
                         <Form.Control type="file" />
                     </Form.Group>
-
-                    <Button variant="primary" type="submit">
+                    <div className="row">
+                             <Button variant="primary" type="submit">
                         Submit
                     </Button>
-                </Form>
-            </div>
+                    </div>
+                   
+                    </Form>
+                    <br/>
+                        </Card>
+                    </div>
+                </div>
+                
+                </div>
         );
     }
 }

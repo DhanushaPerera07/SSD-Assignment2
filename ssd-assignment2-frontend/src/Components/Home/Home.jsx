033e4ec56693d40c7dbb7c7d6da68ec2c84f3523
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2021-10-01 17:17:34 +0530

    Login page enhancments

--- ssd-assignment2-frontend/src/Components/Home/Home.jsx
+++ ssd-assignment2-frontend/src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
-import {AuthContext} from '../../context/auth.context';
-import {Button} from 'react-bootstrap';
+import { AuthContext } from '../../context/auth.context';
+import { Button, Form, Card } from 'react-bootstrap';
 
 export default class Home extends React.Component {
 
@@ -24,39 +24,77 @@ export default class Home extends React.Component {
     responseGoogle = (response) => {
         console.log(response);
         console.log(response.profileObj);
-        this.setState({email: response.profileObj.email});
-        this.setState({userName: response.profileObj.name});
-        this.setState({googleId: response.profileObj.googleId});
-        this.setState({imageUrl: response.profileObj.imageUrl});
+        this.setState({ email: response.profileObj.email });
+        this.setState({ userName: response.profileObj.name });
+        this.setState({ googleId: response.profileObj.googleId });
+        this.setState({ imageUrl: response.profileObj.imageUrl });
 
 
     };
 
     render() {
-        const {email} = this.state;
-        const {userName} = this.state;
-        const {googleId} = this.state;
-        const {imageUrl} = this.state;
+        const { email } = this.state;
+        const { userName } = this.state;
+        const { googleId } = this.state;
+        const { imageUrl } = this.state;
 
         return (
             <div>
-                <h1>Book your Doctor</h1>
-                <GoogleLogin
-                    clientId="685011427244-5rn6dm5o8c5r416hk4e4d5dtof5h543o.apps.googleusercontent.com"
-                    buttonText="Login"
-                    onSuccess={this.responseGoogle}
-                    onFailure={this.responseGoogle}
-                    cookiePolicy={'single_host_origin'}
+                <div className="row" style={{ marginTop: '10%' }}>
+                    <div className="col-md-4"></div>
 
-                />
+                    <div className="col-md-4">
+
+                        <h1 style={{ textAlign: 'center' }}>Book your Doctor</h1>
+                        <h2 style={{ textAlign: 'center' }}>Login</h2>
+                        <Card className="container" style={{ padding: '10%' }}>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                    <Form.Text className="text-muted">
+                                        We'll never share your email with anyone else.
+                                    </Form.Text>
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+
+                                <hr />
+                                <h5 style={{ textAlign: 'center' }}>Login with Google</h5>
+
+                                <div style={{textAlign:'center'}}>
+
+                                    <GoogleLogin
+                                        clientId="685011427244-5rn6dm5o8c5r416hk4e4d5dtof5h543o.apps.googleusercontent.com"
+                                        buttonText="Login"
+                                        onSuccess={this.responseGoogle}
+                                        onFailure={this.responseGoogle}
+                                        cookiePolicy={'single_host_origin'}
+                                    />
+                                </div>
+                            </Form>
+                        </Card>
+                    </div>
+                </div>
+
+
                 <h5>{email}</h5>
                 <h5>{userName}</h5>
                 <h5>{googleId}</h5>
                 <h5>{imageUrl}</h5>
                 <img src={imageUrl} alt="Italian Trulli"></img>
 
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <Button variant={'primary'} onClick={this.context.getUserProfileDetails}>Get Google User Details</Button>
             </div>
         );
